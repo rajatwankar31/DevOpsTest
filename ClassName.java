d5e52d6a71a8ec6cf2f71d34b538fb8d522694a2
rajatwankar31 <52561735+rajatwankar31@users.noreply.github.com>
2019-07-09 10:02:52 +0530

    uploaded on GIT for jenkins linking ver-1.0
    we are going to upload and link java file with jenkins so in next update we will write all steps for linking with jenkins

---
+++ ClassName.java
@@ -0,0 +1,13 @@
+public class ClassName
+{
+	public static void main(String[] args)
+	{
+	 String myNa = "rajat";
+	 int myAge = 25;
+	 
+		System.out.println("name is: " + myNa +
+							"age is: " + myAge);
+							
+	return ;
+	}
+}
